f9cb44608eb6ec0e46cbbe3fd3912700b038e6eb
schall11 <shall1@utah.gov>
2016-08-08 19:24:08 -0600

    Change url

--- js/baselayers.js
+++ js/baselayers.js
@@ -2,24 +2,24 @@
 *    Return common layers used in different examples
 */
 function getCommonBaseLayers(map){
-    var topo = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    var topo = L.tileLayer('//server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 });
-    var bathymetryLayer = L.tileLayer.wms("http://ows.emodnet-bathymetry.eu/wms", {
+    var bathymetryLayer = L.tileLayer.wms("//ows.emodnet-bathymetry.eu/wms", {
         layers: 'emodnet:mean_atlas_land',
         format: 'image/png',
         transparent: true,
         attribution: "EMODnet Bathymetry",
         opacity: 0.8
     });
-    var coastlinesLayer = L.tileLayer.wms("http://ows.emodnet-bathymetry.eu/wms", {
+    var coastlinesLayer = L.tileLayer.wms("//ows.emodnet-bathymetry.eu/wms", {
         layers: 'coastlines',
         format: 'image/png',
         transparent: true,
         attribution: "EMODnet Bathymetry",
         opacity: 0.8
     });
-	var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	var Esri_WorldImagery = L.tileLayer('//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 	});
     var bathymetryGroupLayer = L.layerGroup([bathymetryLayer, coastlinesLayer]);    
